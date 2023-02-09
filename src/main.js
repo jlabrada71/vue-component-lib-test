3063871ba88a3e23c4b92308433a357b2fef9340
jlabrada <jlabrada@yahoo.com>
2023-02-08 21:21:08 -0300

    Working test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import labradaComponents from 'labrada-components'
-import '../node_modules/labrada-components/dist/styles.css'
-
-import './assets/main.css'
+import 'labrada-components/styles'
 
 createApp(App).use(labradaComponents).mount('#app')
